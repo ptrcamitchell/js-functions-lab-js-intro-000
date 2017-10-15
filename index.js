db9e9752bbfd289e2342e0bb1c9b4bb25159254f
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-15 19:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,18 @@ function happyHolidaysTo(name){
 function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+
+describe('happyHolidayTo(holiday, name)', () => {
+  it('returns "Happy ${holiday}, ${name}!"', () => {
+    expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+  })
+})
+
+describe('holidayCountdown(holiday, days)', () => {
+  it('returns "It\'s ${days} days until ${holiday}!"', () => {
+    expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+  })
+})
+})
+
 */
