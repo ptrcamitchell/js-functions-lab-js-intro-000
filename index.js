4e48187c1090722309f30c4f8e8612f02f42f53c
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-15 20:01:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,3 @@ function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
 }
 
-/*
-describe('holidayCountdown(holiday, days)', () => {
-  it('returns "It\'s ${days} days until ${holiday}!"', () => {
-    expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-  })
-})
-})
-
-*/
